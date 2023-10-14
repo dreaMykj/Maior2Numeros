6e9bd75d9ccd6b4cb1e75564cad313587f2219e2
dreaMykj <gabrielgreghi2018@gmail.com>
2023-10-14 11:23:47 -0300

    Concluido

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int primeiro, segundo, maior;
+
+Console.Write("Digite o primeiro numero aqui..:");
+primeiro = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o segundo numero aqui...:");
+segundo = Convert.ToInt32(Console.ReadLine());
+
+if (primeiro > segundo)
+{
+    maior = primeiro;
+}
+else
+{
+    maior = segundo;
+}
+
+Console.WriteLine($"O maior numero é {maior}");
